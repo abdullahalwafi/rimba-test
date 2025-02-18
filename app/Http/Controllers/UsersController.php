28cf06f426b729125a44b5a830eb888a1f2507ed
Abdullah Al Wafi <abdullahalwafi16@gmail.com>
2025-02-18 14:27:16 +0700

    Feat: show user

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -47,7 +47,15 @@ class UsersController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id) {}
+    public function show(string $id) {
+        $user = User::find($id);
+
+        if (!$user) {
+            return response()->json(['message' => 'User not found'], 404);
+        }
+
+        return response()->json(['message' => 'User found', 'data' => $user]);
+    }
 
     /**
      * Show the form for editing the specified resource.
